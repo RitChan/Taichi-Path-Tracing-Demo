c916e5e0729321658b156a449c1388722c8c0f2f
chenh <chenh0510@qq.com>
2022-08-01 20:02:55 +0800

    support emitting

--- rendering.py
+++ rendering.py
@@ -5,11 +5,21 @@ import numpy as np
 import taichi as ti
 import taichi.types
 
-# Self-defined types
+# Zero Flag
+ZERO = 0x00000000
+
+# Self-defined Types
 Vector3f = ti.types.vector(3, ti.float32)
 Ray = ti.types.struct(o=Vector3f, d=Vector3f)
-HitRecord = ti.types.struct(hit=ti.int32,
-                            t=ti.float32)  # currently, flag only indicates whether a ray hit a triangle or not
+HitRecord = ti.types.struct(flag=ti.int32, t=ti.float32, object_idx=ti.int32, material=ti.int32)
+PointSample = ti.types.struct(pos=Vector3f, prob=ti.float32)
+
+# HitRecord Flags
+HIT = 0x00000001
+IS_LIGHT = 0x00000002
+
+# Material Type
+# TODO
 
 
 @ti.data_oriented
@@ -45,24 +55,57 @@ class RenderingKernel:
             rel_x = i_f / float(self.canvas.width)
             rel_y = j_f / float(self.canvas.height)
             view_ray = self.camera.ray_cast(rel_x, rel_y)
-            min_t = 1000.1
-            hit = 0
-            for k in range(self.triangles.shape[0]):
-                v0 = self.triangles[k][0, :].transpose()
-                v1 = self.triangles[k][1, :].transpose()
-                v2 = self.triangles[k][2, :].transpose()
-                n = (v1 - v0).cross(v2 - v0)
-                hit_record = ray_triangle_intersection(view_ray.o, view_ray.d, v0, v1, v2, n, 0.1, 1000.0)
-                if hit_record.hit == 1:
-                    hit = 1
-                    if hit_record.t < min_t:
-                        min_t = hit_record.t
-            if hit == 1:
-                self.canvas.buffer[i, j] = ti.Vector([min_t, min_t, min_t], ti.f32)
+            hit_record = self.ray_hit_nearest(view_ray, 0.01, 1000.0)
+            if flag_query(hit_record.flag, HIT) == 1:
+                # self.canvas.buffer[i, j] = ti.Vector([hit_record.t, hit_record.t, hit_record.t], ti.f32)
+                if flag_query(hit_record.flag, IS_LIGHT) == 1:
+                    light = self.lights[0]
+                    self.canvas.buffer[i, j] = light.radiance(-view_ray.d) * light.color
 
     @ti.func
     def sample(self, x, k_o):
         pass
+
+    @ti.func
+    def direct_light_radiance_at(self, x, k_o, normal, material):
+        result = float(0)
+        k_o = k_o * ti.rsqrt(k_o.transpose() @ k_o)[0]
+        for i in ti.static(range(len(self.lights))):
+            light = self.lights[i]
+            sample = light.sample()
+            k_i = sample.pos - x
+            dist_sqr = k_i.transpose() @ k_i
+            dist = ti.sqrt(dist_sqr)
+            k_i = k_i / dist
+            shadow_ray = Ray(o=x, d=k_i)
+            hit_record = self.ray_hit_nearest(shadow_ray, 0.1, dist + 1)
+            if flag_query(hit_record.flag, HIT) and hit_record.t > dist - 1e-3:
+                radiance = light.radiance(-k_i)
+                cos_light = (light.normal.transpose() @ (-k_i))[0]
+                cos_x = (normal.transpose() @ (-k_i))[0] * ti.rsqrt(normal.transpose() @ normal)[0]
+                result = result + brdf(material, k_i, k_o) * radiance * cos_x * cos_light / (dist_sqr * sample.prob)
+        return result
+
+    @ti.func
+    def ray_hit_nearest(self, ray, t0, t1):
+        result = HitRecord(flag=ZERO, t=t1, object_idx=-1, material=-1)
+        for i in range(self.triangles.shape[0]):
+            v0 = self.triangles[i][0, :].transpose()
+            v1 = self.triangles[i][1, :].transpose()
+            v2 = self.triangles[i][2, :].transpose()
+            n = (v1 - v0).cross(v2 - v0)
+            hit_record = ray_triangle_intersection(ray.o, ray.d, v0, v1, v2, n, t0, t1)
+            if flag_query(hit_record.flag, HIT) == 1 and hit_record.t < result.t:
+                result.flag = flag_set(result.flag, HIT)
+                result.t = hit_record.t
+                result.object_idx = i
+        for i in ti.static(range(len(self.lights))):
+            hit_record = self.lights[i].ray_hit(ray, t0, t1)
+            if flag_query(hit_record.flag, HIT) == 1 and hit_record.t < result.t:
+                result.flag = flag_set(result.flag, HIT | IS_LIGHT)
+                result.t = hit_record.t
+                result.object_idx = i
+        return result
 
 
 @ti.data_oriented
@@ -101,8 +144,59 @@ class Camera:
 
 @ti.data_oriented
 class Light:
-    def __init__(self):
-        self.x = ti.Vector([1, 2, 3], dt=ti.i32)
+    """Simple area light"""
+
+    def __init__(self, origin: Sequence, dir_x: Sequence, dir_y: Sequence, power: float, color: Sequence = (1, 1, 1)):
+        """
+        :param origin: 左下角
+        :param dir_x: 宽
+        :param dir_y: 高
+
+        normal = dir_x cross dir_y
+        """
+        self.origin = ti.Vector(origin, dt=ti.f32)
+        self.dir_x = ti.Vector(dir_x, dt=ti.f32)
+        self.dir_y = ti.Vector(dir_y, dt=ti.f32)
+        self.normal = ti.Vector(np.cross(dir_x, dir_y) / np.linalg.norm(np.cross(dir_x, dir_y)), dt=ti.f32)
+        self.area: ti.float32 = np.linalg.norm(self.dir_x) * np.linalg.norm(self.dir_y)
+        self.irradiance: ti.f32 = power / self.area
+        self.color = ti.Vector(color, dt=ti.f32)
+
+    @ti.func
+    def ray_hit(self, ray, t0, t1):
+        result = HitRecord(flag=ZERO, t=0, object_idx=-1, material=ZERO)
+        o_diff = ray.o - self.origin
+        M = ti.Matrix([
+            [self.dir_x[0], self.dir_y[0], -ray.d[0]],
+            [self.dir_x[1], self.dir_y[1], -ray.d[1]],
+            [self.dir_x[2], self.dir_y[2], -ray.d[2]]])
+        x = M.inverse() @ o_diff
+        if 0 < x[0] < 1 and 0 < x[1] < 1 and t0 < x[2] < t1:
+            result.flag = flag_set(result.flag, HIT | IS_LIGHT)
+            result.t = x[2]
+        return result
+
+    @ti.func
+    def sample(self):
+        n0 = ti.random(ti.f32)
+        n1 = ti.random(ti.f32)
+        pos = self.origin + n0 * self.dir_x + n1 * self.dir_y
+        prob = 1 / self.area
+        result = PointSample(pos=pos, prob=prob)
+        return result
+
+    @ti.func
+    def probability(self, x) -> ti.f32:
+        return 1 / self.area
+
+    @ti.func
+    def radiance(self, direction):
+        cos = (self.normal.transpose() @ direction)[0] * ti.rsqrt(direction.transpose() @ direction)[0]
+        sin = 1 - cos * cos
+        result: ti.f32 = 0
+        if cos > 1e-3:
+            result = self.irradiance / (2 * float(3.141593) * sin)
+        return result
 
 
 @ti.data_oriented
@@ -131,21 +225,45 @@ class Canvas:
 
 @ti.func
 def ray_triangle_intersection(o, d, v0, v1, v2, n, t0, t1):
-    hit_record = HitRecord(hit=1, t=0)
+    hit_record = HitRecord(flag=HIT, t=0, object_idx=-1, material=-1)
     a = o - v0
     b = v1 - v0
     c = v2 - v0
     if ti.abs((a.transpose() @ n)[0]) < 1e-3:  # d与n平行, 我们认为没有交点
-        hit_record.hit = 0
-    if hit_record.hit == 1:
+        hit_record.flag = flag_clear(hit_record.flag, HIT)
+    if flag_query(hit_record.flag, HIT) == 1:
         M = ti.Matrix([
             [b[0], c[0], -d[0]],
             [b[1], c[1], -d[1]],
             [b[2], c[2], -d[2]]])
         x = M.inverse() @ a
         alpha = 1 - x[0] - x[1]
-        if 0 < x[0] < 1 and 0 < x[1] < 1 and 0 < alpha < 1:
+        if 0 < x[0] < 1 and 0 < x[1] < 1 and 0 < alpha < 1 and t0 < x[2] < t1:
             hit_record.t = x[2]
         else:
-            hit_record.hit = 0
+            hit_record.flag = flag_clear(hit_record.flag, HIT)
     return hit_record
+
+
+@ti.func
+def brdf(material: ti.i32, k_i, k_o):
+    """假设半球在y正半轴, 底部紧贴xz平面"""
+    return float(1 / 3.1415926)  # perfect diffusing
+
+
+@ti.func
+def flag_set(status: ti.i32, flag_type: ti.i32):
+    return status | flag_type
+
+
+@ti.func
+def flag_clear(status: ti.i32, flag_type: ti.i32):
+    return status & (~flag_type)
+
+
+@ti.func
+def flag_query(status: ti.i32, flag_type: ti.i32) -> ti.i32:
+    result: ti.i32 = 0
+    if status & flag_type == flag_type:
+        result = 1
+    return result
--- rendering_demo.py
+++ rendering_demo.py
@@ -8,20 +8,17 @@ if __name__ == '__main__':
 
     WIDTH, HEIGHT = 300, 300
     triangles = [[-0.5, -0.5, -1], [0.5, -0.5, -1], [0, 0.5, -5]]
+    lights = [Light(origin=[-0.5, -0.5, -2], dir_x=[1, 0, 0], dir_y=[0, 0.5, 1], power=500)]
     camera = Camera(eye=[0, 0, 0], forward=[0, 0, -1], up=[0, 1, 0], aspect_ratio=WIDTH / HEIGHT, fov_degree=60)
-    kernel = RenderingKernel(triangles, [0], [], camera, Canvas(WIDTH, HEIGHT))
+    kernel = RenderingKernel(triangles, [0], lights, camera, Canvas(WIDTH, HEIGHT))
 
     kernel.canvas.clear(ti.Vector([0, 0, 0], dt=ti.float32))
     kernel.render()
     pixels = kernel.canvas.buffer.to_numpy()
     max_depth = float(np.max(pixels))
-
-    kernel.canvas.clear(ti.Vector([max_depth, max_depth, max_depth], dt=ti.float32))
-    kernel.render()
-    pixels = kernel.canvas.buffer.to_numpy()
-    kernel.canvas.normalize_depth(float(np.max(pixels)), float(np.max(pixels) - np.min(pixels)))
+    pixels = pixels / max_depth
 
     gui = ti.GUI("Triangle", res=(WIDTH, HEIGHT))
     while gui.running:
-        gui.set_image(kernel.canvas.buffer)
+        gui.set_image(pixels)
         gui.show()
